2e32dd3174daf5b03089450612a82a577d69ccf5
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-06-11 00:01:05 -0400

    lol

--- auto_announcer/eventhandler.py
+++ auto_announcer/eventhandler.py
@@ -12,25 +12,28 @@ class event_handler(commands.Cog):
     def unload(self):
         self.auto_announcer.cancel()
 
-    @tasks.loop(hours=1)
+    @tasks.loop(seconds=1)
     async def auto_announcer(self):
         await self.client.wait_until_ready()
         try:
             with open("servers.json") as f:
                 servers = json.load(f)
 
-            channel = self.client.get_channel(AUTO_ANNOUNCE_CHANNEL)            
-            msg = random.randint(0, len(servers[str(channel.guild.id)]["auto_messages"]))
-            msg_id = list(servers[str(channel.guild.id)]["auto_messages"].keys())[msg]
-            msg_data = servers[str(channel.guild.id)]["auto_messages"][msg_id]
+            channel = self.client.get_channel(AUTO_ANNOUNCE_CHANNEL)
+            last_msg = await channel.fetch_message(channel.last_message_id)
+            if last_msg.author != self.client:
+                msg = random.randint(0, len(servers[str(channel.guild.id)]["auto_messages"]))
+                msg_id = list(servers[str(channel.guild.id)]["auto_messages"].keys())[msg]
+                msg_data = servers[str(channel.guild.id)]["auto_messages"][msg_id]
 
-            if servers[str(channel.guild.id)]["auto_announcer"]:
-                embed=discord.Embed(
-                    title=msg_data["title"],
-                    description=msg_data["desc"]
-                )
-                await channel.send(embed=embed)
+                if servers[str(channel.guild.id)]["auto_announcer"]:
+                    embed=discord.Embed(
+                        title=msg_data["title"],
+                        description=msg_data["desc"]
+                    )
+                    await channel.send(embed=embed)
         except:
+            raise
             pass
 
 def setup(client):
