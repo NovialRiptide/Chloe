6d213d0a096002d362c299722a60501a50248490
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2020-07-15 22:44:24 -0400

    tutor restriction update

---
+++ commands/tutorban.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from vars import *
+
+class sessions(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def tutorban(self, ctx, member: discord.Member, *, reason):
+        try:
+            for role in TUTOR_ROLES:
+                await ctx.author.remove_roles(ctx.guild.get_role(role))
+            await ctx.author.remove_roles(ctx.guild.get_role(MAIN_TUTOR_ROLE))
+        except: pass
+
+        await member.send(f"{ctx.author} has restricted you from becoming a tutor.")
+        await ctx.send(f"Restricted {member.mention} from becoming a tutor for {reason}")
+
+def setup(bot):
+    bot.add_cog(sessions(bot))
--- sessions/tutor_assign.py
+++ sessions/tutor_assign.py
@@ -11,6 +11,7 @@ class sessions(commands.Cog):
         if subject.upper() in TUTOR_ROLES.keys() and ctx.guild.get_role(BANNED_ASSIGN_TUTOR_ROLE) not in ctx.author.roles:
             if ctx.guild.get_role(MAIN_TUTOR_ROLE) not in ctx.author.roles:
                 await ctx.author.add_roles(ctx.guild.get_role(MAIN_TUTOR_ROLE))
+                
             await ctx.author.add_roles(ctx.guild.get_role(TUTOR_ROLES[subject.upper()]))
             await ctx.send(f"You became a {subject} tutor!")
         elif ctx.guild.get_role(BANNED_ASSIGN_TUTOR_ROLE) in ctx.author.roles:
